7b8e9489da292a84886536a129b091ededdfbd5e
elomir <elomir.fj@live.com>
2020-10-20 00:35:09 -0300

    feat: added score and fix readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 a snake game made with python
 
 Many OS have their own packages of Pygame, you'll find how install this packages in this URL https://www.pygame.org/wiki/GettingStarted
-After installed the packages, install the requirements with pip install -r requirements.txt, so you can renjoy the game. 
+After installed the packages, install the requirements with pip install -r requirements.txt, so you can enjoy the game. 
--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
-import pygame, random
+import pygame
+import random
 from pygame.locals import *
-
+from pygame import font as pygame_font
 
 UP = 0
 RIGHT = 1
@@ -14,7 +15,7 @@ def on_grid_random():
     """
     x = random.randint(0, 590)
     y = random.randint(0, 590)
-    return (x // 10 * 10, y // 10 * 10)
+    return x // 10 * 10, y // 10 * 10
 
 
 def wall_limit(snake_posit):
@@ -46,7 +47,8 @@ def collision_snake_apple(c1, c2):
     """This function generate the collision of the snake with the apple
 
     """
-    return (c1[0] == c2[0] and c1[1] == c2[1])
+    return c1[0] == c2[0] and c1[1] == c2[1]
+
 
 def main():
     """This function is responsible for run the game
@@ -57,7 +59,7 @@ def main():
     pygame.display.set_caption('snake game')
 
     snake = [(200, 200), (210, 200), (220, 200)]
-    snake_skin = pygame.Surface((10,10))
+    snake_skin = pygame.Surface((10, 10))
     snake_skin.fill((255, 255, 255))
 
     apple_pos = on_grid_random()
@@ -66,8 +68,13 @@ def main():
 
     my_direction = LEFT
 
-    clock = pygame.time.Clock()
+    pygame_font.init()
+    sysfont = pygame_font.Font(None, 30)
+    screen.fill((10, 10, 10))
+    score = sysfont.render('score: ', True, (255, 255, 255))
 
+    plus_one = 0
+    clock = pygame.time.Clock()
 
     while True:
         clock.tick(10)
@@ -88,24 +95,32 @@ def main():
         if collision_snake_apple(snake[0], apple_pos):
             apple_pos = on_grid_random()
             snake.append((0, 0))
+            plus_one += 1
+
+        count = sysfont.render(str(plus_one), True, (255, 255, 255))
 
         if my_direction == UP:
             snake[0] = (snake[0][0], snake[0][1] - 10)
             if wall_limit(snake[0]) != 0:
-                pygame.quit() 
-        if my_direction == DOWN:
-            snake[0] = (snake[0][0], snake[0][1] + 10)  
-        if my_direction == RIGHT:
-           snake[0] = (snake[0][0] + 10, snake[0][1]) 
-        if my_direction == LEFT:
-           snake[0] = (snake[0][0] - 10, snake[0][1])
+                pygame.quit()
 
-        for posit in range(len(snake) -1, 0, -1):
-                snake[posit] = (snake[posit-1][0], snake[posit-1][1])
-                if wall_limit(snake[posit]) != 0:
-                    pygame.quit()
-        
+        if my_direction == DOWN:
+            snake[0] = (snake[0][0], snake[0][1] + 10)
+
+        if my_direction == RIGHT:
+            snake[0] = (snake[0][0] + 10, snake[0][1])
+
+        if my_direction == LEFT:
+            snake[0] = (snake[0][0] - 10, snake[0][1])
+
+        for posit in range(len(snake) - 1, 0, -1):
+            snake[posit] = (snake[posit - 1][0], snake[posit - 1][1])
+            if wall_limit(snake[posit]) != 0:
+                pygame.quit()
+
         screen.fill((0, 0, 0))
+        screen.blit(score, (1, 1))
+        screen.blit(count, (65, 1))
         screen.blit(apple, apple_pos)
         for position in snake:
             screen.blit(snake_skin, position)
